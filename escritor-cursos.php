8ed29c938392c07a56193b340ba0f54a3fedc754
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-09 16:20:10 -0300

    Escrever arquivos com a função file_put_contents.

--- escritor-cursos.php
+++ escritor-cursos.php
@@ -1,27 +1,22 @@
 <?php
 
-$arquivo = fopen('cursos-php.txt', 'w');
-
 $curso = "Design Patterns PHP I: Boas práticas de programação";
 
-// A função fwrite escreve em um arquivo o conteúdo do segundo parâmetro.
-fwrite($arquivo, $curso);
-// fwrite($arquivo, $curso, 10); // Escreve só 10 caracteres.
+// A função file_put_contents abstrai a escrita em arquivos.
+// O primeiro parâmetro é o nome do arquivo.
+// O segundo parâmetro é o conteúdo para ser escrito.
+// O terceiro parâmetro é uma flag para o modo de arquivos.
+file_put_contents('cursos-php.txt', "Conteúdo do arquivo.");
 
-fclose($arquivo);
-
-// Os modos de abertura de arquivo influenciam no posicionamento do cursor 
-// para escrever o conteúdo dos arquivos.
-
-// O modo w significa "write" e posiciona o cursor no início do arquivo. Apaga arquivo original.
-// O modo r significa "read" e posiciona o cursor no início do arquivo.
-// O modo a significa "append" e posiciona o cursor no final do arquivo.
-// O modo x posiciona o cursor no final do arquivo. Vai falhar se o arquivo já existe.
-// Acrescentando o sinal de + aumenta a permissão para leitura e escrita.
-$arquivo = fopen('cursos-php.txt', 'a');
+// Esta segunda gravação apaga o conteúdo original do arquivo.
+file_put_contents('cursos-php.txt', $curso); 
 
 $curso = "\nDesign Patterns PHP II: Boas práticas de programação";
 
-fwrite($arquivo, $curso);
-
-fclose($arquivo);
+// Repare no terceiro parâmetro, a flag do tipo int FILE_APPEND.
+// Você pode "somar" flags usando o operador binário pipe (|).
+//    FILE_APPEND: Constante para inserir conteúdo a partir do fim do arquivo.
+//    FILE_TEXT: Constante para inserir conteúdo do tipo texto no arquivo. 
+//               FILE_TEXT é uma flag obsoleta no PHP 8.
+// Outras flags: https://www.php.net/manual/pt_BR/function.file-put-contents.php
+file_put_contents('cursos-php.txt', $curso, FILE_APPEND | FILE_TEXT); 
