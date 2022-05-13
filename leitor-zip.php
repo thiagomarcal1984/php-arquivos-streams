fd42ae785b6cb045dea1f83a1a0460d49e6ce85d
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-13 13:40:44 -0300

    Lendo zip com senha.

--- leitor-zip.php
+++ leitor-zip.php
@@ -9,3 +9,30 @@ echo file_get_contents("$protocolo://$arquivo#$subarquivo");
 
 // É possível escrever um arquivo ftp usando file_put_contents!
 // file_put_contents("ftp://servidor.com", "Conteúdo do arquivo.");
+
+
+echo PHP_EOL . PHP_EOL . "Lendo arquivos .zip com senha: " . PHP_EOL;
+$protocolo = "zip";
+$arquivo = "arquivos-com-senha.zip";
+$subarquivo = "lista-cursos.txt";
+$contexto = stream_context_create([
+    'zip' => [
+        'password' => '123'
+    ]
+]);
+echo file_get_contents(
+    "$protocolo://$arquivo#$subarquivo", 
+    false, 
+    $contexto
+);
+
+echo PHP_EOL . PHP_EOL . PHP_EOL;
+echo "É possível fazer o mesmo com a função fopen('wrapper://stream', 'modo', false, 'array de contexto');";
+echo PHP_EOL;
+
+$stream = fopen("$protocolo://$arquivo#$subarquivo", 'r', false, $contexto);
+while(!feof($stream)) {
+    $curso = fgets($stream);
+    echo $curso;
+}
+fclose($stream);
